c6681bab9d4b7c652a19839e644416e1f44e6dd0
junghyuk-yum <62980798+junghyuk-yum@users.noreply.github.com>
2022-09-14 05:33:10 +0000

    Add files via upload

---
+++ BCQ/main.py
@@ -0,0 +1,264 @@
+import argparse
+import gym
+import os
+import matplotlib.pyplot as plt
+
+import BCQ
+import DDPG
+
+import numpy as np
+import torch
+
+
+class ReplayBuffer(object):
+    def __init__(self, state_dim, action_dim, device, max_size=int(1e6)):
+        self.max_size = max_size
+        self.ptr = 0
+        self.size = 0
+
+        self.state = np.zeros((max_size, state_dim))
+        self.action = np.zeros((max_size, action_dim))
+        self.next_state = np.zeros((max_size, state_dim))
+        self.reward = np.zeros((max_size, 1))
+        self.not_done = np.zeros((max_size, 1))
+
+        self.device = device
+
+    def add(self, state, action, next_state, reward, done):
+        self.state[self.ptr] = state
+        self.action[self.ptr] = action
+        self.next_state[self.ptr] = next_state
+        self.reward[self.ptr] = reward
+        self.not_done[self.ptr] = 1. - done
+
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample(self, batch_size):
+        ind = np.random.randint(0, self.size, size=batch_size)
+
+        return (
+            torch.FloatTensor(self.state[ind]).to(self.device),
+            torch.FloatTensor(self.action[ind]).to(self.device),
+            torch.FloatTensor(self.next_state[ind]).to(self.device),
+            torch.FloatTensor(self.reward[ind]).to(self.device),
+            torch.FloatTensor(self.not_done[ind]).to(self.device)
+        )
+
+    def save(self, save_folder):
+        np.save(f"{save_folder}_state.npy", self.state[:self.size])
+        np.save(f"{save_folder}_action.npy", self.action[:self.size])
+        np.save(f"{save_folder}_next_state.npy", self.next_state[:self.size])
+        np.save(f"{save_folder}_reward.npy", self.reward[:self.size])
+        np.save(f"{save_folder}_not_done.npy", self.not_done[:self.size])
+        np.save(f"{save_folder}_ptr.npy", self.ptr)
+
+    def load(self, save_folder, size=-1):
+        reward_buffer = np.load(f"{save_folder}_reward.npy")
+
+        size = min(int(size), self.max_size) if size > 0 else self.max_size
+        self.size = min(reward_buffer.shape[0], size)
+
+        self.state[:self.size] = np.load(f"{save_folder}_state.npy")[:self.size]
+        self.action[:self.size] = np.load(f"{save_folder}_action.npy")[:self.size]
+        self.next_state[:self.size] = np.load(f"{save_folder}_next_state.npy")[:self.size]
+        self.reward[:self.size] = reward_buffer[:self.size]
+        self.not_done[:self.size] = np.load(f"{save_folder}_not_done.npy")[:self.size]
+
+def interact_with_environment(env, state_dim, action_dim, max_action, device, args):
+
+    setting = f"{args.env}_{args.seed}"
+    buffer_name = f"{args.buffer_name}_{setting}"
+
+    policy = DDPG.DDPG(state_dim, action_dim, max_action, device)
+    if args.generate_buffer and not(args.generate_buffer) : policy.load(f"./models/behavioral_{setting}")
+
+    replay_buffer = ReplayBuffer(state_dim, action_dim, device)
+
+    evaluations = []
+
+    state, done = env.reset(), False
+    episode_reward = 0
+    episode_timesteps = 0
+    episode_num = 0
+
+    for t in range(int(args.max_timesteps)):
+
+        episode_timesteps += 1
+
+        if (
+                (args.generate_buffer and np.random.uniform(0, 1) < args.rand_action_p) or
+                (args.train_behavioral and t < args.start_timesteps)
+        ):
+            action = env.action_space.sample()
+        else:
+            action = (
+                    policy.select_action(np.array(state))
+                    + np.random.normal(0, max_action * args.gaussian_std, size=action_dim)
+            ).clip(-max_action, max_action)
+
+        next_state, reward, done, _ = env.step(action)
+        done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+
+        replay_buffer.add(state, action, next_state, reward, done_bool)
+
+        state = next_state
+        episode_reward += reward
+
+        if args.train_behavioral and t >= args.start_timesteps:
+            policy.train(replay_buffer, args.batch_size)
+
+        if done:
+            state, done = env.reset(), False
+            episode_reward = 0
+            episode_timesteps = 0
+            episode_num += 1
+
+        if args.train_behavioral and (t + 1) % args.eval_freq == 0:
+            print("Timestep " + str(t + 1) + " ", end="")
+            evaluations.append(eval_policy(policy, args.env, args.seed))
+            np.save(f"./results/behavioral_{setting}", evaluations)
+            policy.save(f"./models/behavioral_{setting}")
+
+    if args.train_behavioral and not(args.generate_buffer):
+        policy.save(f"./models/behavioral_{setting}")
+
+    elif args.train_behavioral and args.generate_buffer:
+        policy.save(f"./models/behavioral_{setting}")
+        evaluations.append(eval_policy(policy, args.env, args.seed))
+        np.save(f"./results/buffer_performance_{setting}", evaluations)
+        replay_buffer.save(f"./buffers/{buffer_name}")
+
+    else:
+        evaluations.append(eval_policy(policy, args.env, args.seed))
+        np.save(f"./results/buffer_performance_{setting}", evaluations)
+        replay_buffer.save(f"./buffers/{buffer_name}")
+
+def train_BCQ(state_dim, action_dim, max_action, device, args):
+
+    setting = f"{args.env}_{args.seed}"
+    buffer_name = f"{args.buffer_name}_{setting}"
+
+    policy = BCQ.BCQ(state_dim, action_dim, max_action, device, args.discount, args.tau, args.lmbda, args.phi)
+
+    replay_buffer = ReplayBuffer(state_dim, action_dim, device)
+    replay_buffer.load(f"./buffers/{buffer_name}")
+
+    evaluations = []
+    episode_num = 0
+    done = True
+    training_iters = 0
+
+    while training_iters < args.max_timesteps:
+        pol_vals = policy.train(replay_buffer, iterations=int(args.eval_freq), batch_size=args.batch_size)
+
+        evaluations.append(eval_policy(policy, args.env, args.seed))
+        np.save(f"./results/BCQ_{setting}", evaluations)
+
+        training_iters += args.eval_freq
+
+def eval_policy(policy, env_name, seed, eval_episodes=10):
+    eval_env = gym.make(env_name)
+    eval_env.seed(seed + 100)
+
+    avg_reward = 0.
+    for _ in range(eval_episodes):
+        state, done = eval_env.reset(), False
+        while not done:
+            action = policy.select_action(np.array(state))
+            state, reward, done, _ = eval_env.step(action)
+            avg_reward += reward
+
+    avg_reward /= eval_episodes
+
+    print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
+    print("---------------------------------------------")
+    return avg_reward
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default="Hopper-v3")  # OpenAI gym environment name
+    parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
+    parser.add_argument("--buffer_name", default="Robust")  # Prepends name to filename
+    parser.add_argument("--eval_freq", default=5e3, type=float)  # How often (time steps) we evaluate
+    parser.add_argument("--max_timesteps", default=1e6,
+                        type=int)  # Max time steps to run environment or train for (this defines buffer size)
+    parser.add_argument("--start_timesteps", default=25e3,
+                        type=int)  # Time steps initial random policy is used before training behavioral
+    parser.add_argument("--rand_action_p", default=0.3,
+                        type=float)  # Probability of selecting random action during batch generation
+    parser.add_argument("--gaussian_std", default=0.3,
+                        type=float)  # Std of Gaussian exploration noise (Set to 0.1 if DDPG trains poorly)
+    parser.add_argument("--batch_size", default=100, type=int)  # Mini batch size for networks
+    parser.add_argument("--discount", default=0.99)  # Discount factor
+    parser.add_argument("--tau", default=0.005)  # Target network update rate
+    parser.add_argument("--lmbda", default=0.75)  # Weighting for clipped double Q-learning in BCQ
+    parser.add_argument("--phi", default=0.05)  # Max perturbation hyper-parameter for BCQ
+    parser.add_argument("--train_behavioral", action="store_true")  # If true, train behavioral (DDPG)
+    parser.add_argument("--generate_buffer", action="store_true")  # If true, generate buffer
+    args = parser.parse_args()
+
+    args.max_timesteps = 1000000
+    args.train_behavioral = True
+    args.gaussian_std = 0.1
+    args.generate_buffer = True
+
+    print("---------------------------------------")
+    if args.train_behavioral:
+        print(f"Setting: Training behavioral, Env: {args.env}, Seed: {args.seed}")
+    elif args.generate_buffer:
+        print(f"Setting: Generating buffer, Env: {args.env}, Seed: {args.seed}")
+    else:
+        print(f"Setting: Training BCQ, Env: {args.env}, Seed: {args.seed}")
+    print("---------------------------------------")
+
+
+    if not os.path.exists("./results"):
+        os.makedirs("./results")
+
+    if not os.path.exists("./models"):
+        os.makedirs("./models")
+
+    if not os.path.exists("./buffers"):
+        os.makedirs("./buffers")
+
+    env = gym.make(args.env)
+
+    env.seed(args.seed)
+    env.action_space.seed(args.seed)
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    max_action = float(env.action_space.high[0])
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.train_behavioral and args.generate_buffer:
+        # Batch 2
+        interact_with_environment(env, state_dim, action_dim, max_action, device, args)
+        train_BCQ(state_dim, action_dim, max_action, args)
+    elif args.train_behavioral or args.generate_buffer:
+        # Batch 1 or Batch 3
+        interact_with_environment(env, state_dim, action_dim, max_action, device, args)
+    else:
+        # Batch 1 or Batch 3
+        train_BCQ(state_dim, action_dim, max_action, device, args)
+
+
+setting = f"{args.env}_{args.seed}"
+avg_reward_ddpq = np.load(f"./results/behavioral_{setting}.npy")
+avg_reward_bcq = np.load((f"./results/BCQ_{setting}.npy"))
+
+plt.plot(avg_reward_ddpq)
+plt.plot(avg_reward_bcq)
+plt.xlabel("Episode")
+plt.ylabel("Avg. Epsiodic Reward")
+plt.savefig('BCQ_score_history.png')
+plt.show()
+plt.close()
+
+env.close()
